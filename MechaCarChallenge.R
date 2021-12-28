ccde3f1a6ea9039e6f47985bb2ab98a130da1df2
Jennifer Robert <jrobert2121@gmail.com>
2021-12-28 16:52:33 -0500

    summary statistics on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ mpg_table <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFacto
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_table)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_table))
+
+suspension_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+table_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Is the slope of the linear model considered to be zero  why or why not?
 Does this linear model predict mpg of MechaCar prototypes effectively?  Why or why not?
 
 ## Summary Statistics on Suspension Coils
+write summary using screenshots of both summaries and address the following:
+The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?

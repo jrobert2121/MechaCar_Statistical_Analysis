412ff76e8a1314696be14edc66bb2b790b696d05
Jennifer Robert <jrobert2121@gmail.com>
2021-12-28 17:17:35 -0500

    t tests on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,13 +1,30 @@
 library(dplyr)
 
+# Linear Regression to Predict MPG
+# read csv to dataframe
 mpg_table <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 
+# multiple linear regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_table)
 
+# find p value and r squared
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg_table))
 
+#Suspension Coil Lot Analysis
+#read csv to dataframe
 suspension_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 
+# create summary dataframe
 table_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
+# create summary for each lot
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# T-tests on Suspension Coils
+# across all lots
+t.test(suspension_coil$PSI, mu=1500)
+
+# across each lot
+t.test(subset(suspension_coil, Manufacturing_Lot == 'Lot1')$PSI, mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == 'Lot2')$PSI, mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == 'Lot3')$PSI, mu=1500)
--- README.md
+++ README.md
@@ -14,3 +14,14 @@ Does this linear model predict mpg of MechaCar prototypes effectively?  Why or w
 ## Summary Statistics on Suspension Coils
 write summary using screenshots of both summaries and address the following:
 The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+## T-Tests on Suspension Coils
+briefly summarize findings and my interpretation for the results.  include screenshots
+
+## Study Design: MechaCar vs Competition
+short description of a statistical study that can quantify how the mechacar performs against the competition.  think critically about what metrics would be of interest to a consumer (cost, city or hwy fuel efficiency, hp, maint cost, or safety rating).  Address the following questions:
+ - what metric or metrics will i test?
+ - what is the null or alternative hypothesis?
+ - what statistical test would be used to test the hypothesis and why
+ - what data is needed to run the statistical test
+ 
